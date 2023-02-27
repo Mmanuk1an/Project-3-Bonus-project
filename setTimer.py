d2824f9aec6b81574715a6e19563a13bbd0cf259
TUMO Labs student <tumolabsstudent@TUMOs-iMac.local>
2023-02-27 14:09:25 +0400

    the bonus project is done

---
+++ setTimer.py
@@ -0,0 +1,21 @@
+import time
+
+timer = input('Please insert time to count down (h:m:s or in seconds): ')
+
+def countdown(setTime):
+    for i in range(setTime, -1, -1):
+        sec = i % 60
+        min = int(i / 60) % 60
+        hour = int(i / 3600)
+        time.sleep(1)
+        print(f"{hour:02}:{min:02}:{sec:02}")
+
+
+if ":" in timer:
+    hour, min, sec = timer.split(':')
+    setTime = int(hour) * 3600 + int(min) * 60 + int(sec)
+    countdown(setTime)
+elif timer:
+    countdown(int(timer))
+
+print("Your time is over !")
